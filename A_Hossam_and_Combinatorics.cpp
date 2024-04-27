3d3e9629127a37ad240e371b9328750e036e6d68
MD. Kawsar Mahmud <119809945+KMsojib@users.noreply.github.com>
2024-04-27 10:52:18 +0600

    Add files via upload
    day-2

--- A_Hossam_and_Combinatorics.cpp
+++ A_Hossam_and_Combinatorics.cpp
@@ -7,23 +7,31 @@ int main()
     fast_io;
     int t;  cin>>t;
     while(t--){
-        int n;  cin>>n;
-        vector<int>a(n);
+        ll n;  cin>>n;
+        vector<ll>a(n);
+        ll maxo=0,mini=INT_MAX,maxo_cn = 0,mini_cn =0;
         for (int i = 0; i < n; i++){
             cin>>a[i];
+            maxo = max(maxo,a[i]);
+            mini = min(mini,a[i]);
         }
-        sort(a.begin(),a.end());
-        int max_dif = a[n-1]-a[0];
-        int las_val = a[n-1];
-        
-        int cn = 2;
-        for(int i = 1; i < n-1; i++){
-            if(las_val-a[i] == max_dif){
-                cn+=2;
+
+        for(int i=0;i<n;i++){
+            if(a[i]==maxo){
+                maxo_cn++;
+            }
+            if(a[i]==mini){
+                mini_cn++;
             }
         }
-        cout<<cn<<endl;
-        
+        ll ans=0;
+        if(mini == maxo){
+            ans = (n*(n-1));
+        }
+        else{
+            ans = 2LL*(mini_cn*(maxo_cn));
+        }  
+        cout<<ans<<endl;
     }
 
     return 0;
