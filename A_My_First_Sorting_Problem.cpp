e71b10157887f7e64ce750e5e77b81abac97f1e0
MD. Kawsar Mahmud <119809945+KMsojib@users.noreply.github.com>
2024-05-11 06:54:46 +0600

    Add files via upload
    div 4

---
+++ A_My_First_Sorting_Problem.cpp
@@ -0,0 +1,29 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// typedef long long int ll;
+// #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+// int main()
+// {
+//     fast_io;
+//     int t;  cin>>t;
+//     while(t--){
+//         int a[2];
+//         cin>>a[0]>>a[1];
+//         cout<<min(a[0],a[1])<<" "<<max(a[0],a[1])<<endl;
+//     }
+
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;  cin>>t;
+    while(t--){
+        int x,y;    cin>>x>>y;
+        cout<<min(x,y)<<" "<<max(x,y)<<endl;
+    }
+
+    return 0;
+}
